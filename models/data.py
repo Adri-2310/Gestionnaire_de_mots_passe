e9ac5d5db817caad7805d746e1c5ccf4969b7078
AdrienMertens <mertensadrien23@hotmail.be>
2025-05-21 19:14:35 +0200

    Refactor data handling and enhance update functionality.
    Standardized parameter naming across functions for consistency. Implemented data update logic in `modify_data` and updated `check_if_user_data_exists` to reflect parameter changes. Revised database connection configuration in project settings.

--- models/data.py
+++ models/data.py
@@ -29,7 +29,7 @@ def registre_data(user: str, password: str, source: str) -> bool:
     :return: True si l'insertion a réussi, False si les données existent déjà
     """
     check_if_datas_exists()
-    if not check_if_user_data_exists(new_user=user, new_password=password, new_source=source):
+    if not check_if_user_data_exists(user=user, password=password, source=source):
         conn = sqlite3.connect(r'..\datas.db')
         cursor = conn.cursor()
         cursor.execute('''INSERT INTO data (user, password, source) VALUES (?, ?, ?)''', (user, password, source))
@@ -56,19 +56,27 @@ def remove_data(id_data:str) -> bool:
     conn.close()
     return True
 
-def modify_data(old_user: str, old_password: str, old_source: str, new_user: str, new_password: str, new_source: str) -> bool:
+def modify_data(old_id_user:str, old_user:str ,old_password : str ,old_source :str , new_user: str, new_password: str, new_source: str) -> bool:
     """
     Modifie les données d'un utilisateur dans la base de données.
 
-    :param old_user: Ancien nom d'utilisateur
-    :param old_password: Ancien mot de passe de l'utilisateur
-    :param old_source: Ancienne source associée à l'utilisateur
+    :param old_id_user: Ancienne id associée à l'utilisateur
     :param new_user: Nouveau nom d'utilisateur
     :param new_password: Nouveau mot de passe de l'utilisateur
     :param new_source: Nouvelle source associée à l'utilisateur
     :return: True si la modification a réussi, False sinon
     """
-    pass
+    check_if_datas_exists()
+    if not check_if_user_data_exists(user=old_user, password=old_password, source=old_source):
+        conn = sqlite3.connect(r'..\datas.db')
+        cursor = conn.cursor()
+        cursor.execute('''UPDATE data SET user = ?, password = ?, source = ? WHERE id = ?''', (new_user, new_password, new_source, old_id_user))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return True
+    else:
+        return False
 
 def search_data_all_data_user() -> list:
     """
@@ -108,18 +116,18 @@ def check_if_datas_exists() -> None:
     if not os.path.exists(r'..\datas.db'):
         init_db()
 
-def check_if_user_data_exists(new_user: str, new_password: str, new_source: str) -> bool:
+def check_if_user_data_exists(user: str, password: str, source: str) -> bool:
     """
     Vérifie si les données d'un utilisateur existent déjà dans la base de données.
 
-    :param new_user: Nom d'utilisateur à vérifier
-    :param new_password: Mot de passe à vérifier
-    :param new_source: Source à vérifier
+    :param user: Nom d'utilisateur à vérifier
+    :param password: Mot de passe à vérifier
+    :param source: Source à vérifier
     :return: True si les données existent, False sinon
     """
     conn = sqlite3.connect(r'..\datas.db')
     cursor = conn.cursor()
-    cursor.execute('''SELECT 1 FROM data WHERE user = ? AND password = ? AND source = ?''', (new_user, new_password, new_source))
+    cursor.execute('''SELECT 1 FROM data WHERE user = ? AND password = ? AND source = ?''', (user, password, source))
     exists = cursor.fetchone() is not None
     cursor.close()
     conn.close()
@@ -130,6 +138,5 @@ if __name__ == '__main__':
     registre_data("adrien", "1234", "https://youtube.com")
     registre_data("alicia","1400","facebook.com")
     result = search_data_all_data_user()
-    remove_data("5")
-    result2 = search_data_all_data_user()
-    print(result,"\n", result2)
+
+    print(result,"\n",)
