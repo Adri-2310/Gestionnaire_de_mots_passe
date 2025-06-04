8279dc5241e1f655ded04d4fe25b6f040247c024
AdrienMertens <mertensadrien23@hotmail.be>
2025-06-04 19:21:31 +0200

    transformation du code en object le data.py ok pour l'instant

--- models/data.py
+++ models/data.py
@@ -1,147 +1,122 @@
 __author__ = "Adrien Mertens"
 __version__ = "1.0"
 
-import os.path
 from dataclasses import dataclass, field
 import sqlite3
 from contextlib import closing
 
 @dataclass
 class Data:
-    username : str
-    password : str
-    source : str
+    username: str
+    password: str
+    source: str
     id: int = field(default=-1)
 
 class Datas:
-
     def __init__(self):
         """Constructeur"""
-        self.database = sqlite3.connect(r'..\datas.db')
+        self.database = sqlite3.connect("../db_gestionnaire_password.db")
 
     @property
     def cursor(self) -> sqlite3.Cursor:
-        """Créer le paramètre"""
+        """Créer le curseur"""
         return self.database.cursor()
 
     def commit(self):
-        """Sauvegarde les modifications appliquée à la table"""
+        """Sauvegarde les modifications appliquées à la table"""
         self.database.commit()
 
+    def check_if_user_data_exists(self, username: str, password: str, source: str) -> bool:
+        """
+        Vérifie si les données d'un utilisateur existent déjà dans la base de données.
+        """
+        sql = '''SELECT 1 FROM data WHERE username = ? AND password = ? AND source = ?'''
+        try:
+            with closing(self.cursor) as cursor:
+                cursor.execute(sql, (username, password, source))
+                exists = cursor.fetchone() is not None
+            return exists
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la vérification des données : {e}")
 
 
-def registre_data(user: str, password: str, source: str) -> bool:
-    """
-    Enregistre les données d'un utilisateur dans la base de données si elles n'existent pas déjà.
+    def registre_data(self, username: str, password: str, source: str) -> bool:
+        """
+        Enregistre les données d'un utilisateur dans la base de données si elles n'existent pas déjà.
+        """
+        try:
+            if not self.check_if_user_data_exists(username=username, password=password, source=source):
+                sql = '''INSERT INTO data (username, password, source) VALUES (?, ?, ?)'''
+                with closing(self.cursor) as cursor:
+                    cursor.execute(sql, (username, password, source))
+                self.commit()
+                return True
+            return False
+        except sqlite3.Error as e:
+            print(f"Erreur lors de l'enregistrement des données : {e}")
 
-    :param user: Nom d'utilisateur
-    :param password: Mot de passe de l'utilisateur
-    :param source: Source associée à l'utilisateur
-    :return: True si l'insertion a réussi, False si les données existent déjà
-    """
-    check_if_datas_exists()
-    if not check_if_user_data_exists(user=user, password=password, source=source):
-        conn = sqlite3.connect(r'..\datas.db')
-        cursor = conn.cursor()
-        cursor.execute('''INSERT INTO data (user, password, source) VALUES (?, ?, ?)''', (user, password, source))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return True
-    else:
-        return False
-
-def remove_data(id_data:str) -> bool:
-    """
-    Supprime les données d'un utilisateur de la base de données.
-
-    :param id_data: Id lié à la donnée
-    :return: True si la suppression a réussi, False sinon
-    """
-    check_if_datas_exists()
-    conn = sqlite3.connect(r'..\datas.db')
-    cursor = conn.cursor()
-    cursor.execute('''DELETE FROM data WHERE id = ?''', (id_data,))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return True
-
-def modify_data(old_id_user:str, old_user:str ,old_password : str ,old_source :str , new_user: str, new_password: str, new_source: str) -> bool:
-    """
-    Modifie les données d'un utilisateur dans la base de données.
-
-    :param old_id_user: Ancienne id associée à l'utilisateur
-    :param new_user: Nouveau nom d'utilisateur
-    :param new_password: Nouveau mot de passe de l'utilisateur
-    :param new_source: Nouvelle source associée à l'utilisateur
-    :return: True si la modification a réussi, False sinon
-    """
-    check_if_datas_exists()
-    if not check_if_user_data_exists(user=old_user, password=old_password, source=old_source):
-        conn = sqlite3.connect(r'..\datas.db')
-        cursor = conn.cursor()
-        cursor.execute('''UPDATE data SET user = ?, password = ?, source = ? WHERE id = ?''', (new_user, new_password, new_source, old_id_user))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return True
-    else:
-        return False
-
-def search_data_all_data_user() -> list:
-    """
-    Recherche toutes les données de chaque enregistrement dans la base de données.
-
-    :return: Liste des enregistrements trouvés
-    """
-    check_if_datas_exists()
-    conn = sqlite3.connect(r'..\datas.db')
-    cursor = conn.cursor()
-    cursor.execute('''SELECT * FROM data''')
-    datas = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return datas
-
-def search_data_one_user(id_data : str) -> tuple:
-    """
-    Recherche un enregistrement spécifique de l'utilisateur dans la base de données.
-
-    :param id_data: Id de la donnée
-    :return: Tuple représentant l'enregistrement trouvé, ou None si aucun enregistrement n'est trouvé
-    """
-    check_if_datas_exists()
-    conn = sqlite3.connect(r'..\datas.db')
-    cursor = conn.cursor()
-    cursor.execute('''SELECT * FROM data WHERE id = ?''', (id_data,))
-    data = cursor.fetchone()
-    cursor.close()
-    conn.close()
-    return data
-
-def check_if_datas_exists() -> None:
-    """
-    Vérifie si le fichier de la base de données existe. Si non, initialise la base de données.
-    """
-    if not os.path.exists(r'..\datas.db'):
-        init_db()
-
-def check_if_user_data_exists(user: str, password: str, source: str) -> bool:
-    """
-    Vérifie si les données d'un utilisateur existent déjà dans la base de données.
-
-    :param user: Nom d'utilisateur à vérifier
-    :param password: Mot de passe à vérifier
-    :param source: Source à vérifier
-    :return: True si les données existent, False sinon
-    """
-    conn = sqlite3.connect(r'..\datas.db')
-    cursor = conn.cursor()
-    cursor.execute('''SELECT 1 FROM data WHERE user = ? AND password = ? AND source = ?''', (user, password, source))
-    exists = cursor.fetchone() is not None
-    cursor.close()
-    conn.close()
-    return exists
+    def remove_data(self, id_data: str) -> bool:
+        """
+        Supprime les données d'un utilisateur de la base de données.
+        """
+        try:
+            if self.search_data_one_user(id_data=id_data):
+                sql = '''DELETE FROM data WHERE id = ?'''
+                with closing(self.cursor) as cursor:
+                    cursor.execute(sql, (id_data,))
+                self.commit()
+                return True
+            return False
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la suppression des données : {e}")
 
 
+    def modify_data(self, old_id: int, old_username: str, old_password: str, old_source: str,
+                    new_username: str, new_password: str, new_source: str) -> bool:
+        """
+        Modifie les données d'un utilisateur dans la base de données.
+        """
+        try:
+            if self.check_if_user_data_exists(username=old_username, password=old_password, source=old_source):
+                sql = '''UPDATE data SET username = ?, password = ?, source = ? WHERE id = ?'''
+                with closing(self.cursor) as cursor:
+                    cursor.execute(sql, (new_username, new_password, new_source, old_id))
+                self.commit()
+                return True
+            return False
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la modification des données : {e}")
+
+
+    def search_data_all_data_user(self) -> list:
+        """
+        Recherche toutes les données de chaque enregistrement dans la base de données.
+        """
+        try:
+            sql = '''SELECT * FROM data'''
+            with closing(self.cursor) as cursor:
+                cursor.execute(sql)
+                all_datas = cursor.fetchall()
+            return all_datas
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la recherche des données : {e}")
+            return []
+
+    def search_data_one_user(self, id_data: str) -> tuple:
+        """
+        Recherche un enregistrement spécifique de l'utilisateur dans la base de données.
+        """
+        try:
+            sql = '''SELECT * FROM data WHERE id = ?'''
+            with closing(self.cursor) as cursor:
+                cursor.execute(sql, (id_data,))
+                data = cursor.fetchone()
+            return data
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la recherche de l'utilisateur : {e}")
+            return ()
+
+if __name__ == '__main__':
+    datas = Datas()
+    answer = datas.registre_data(username="test", password="<PASSWORD>", source="test")
+    print(answer)
